045ae498dbc8339a5d080ce4b1a2fb47828ddf8c
Javier Puebla <j.puebla@lab.matcom.uh.cu>
2017-07-04 22:46:43 -0400

    detecting contours

--- detect_home.py
+++ detect_home.py
@@ -1,14 +1,21 @@
 import cv2
+import numpy as np
 from cvinput import cvwindows
 
 def main():
-    camera = cvwindows.create('camera')
     frame = cv2.imread('0.png', 0)
     blur = cv2.GaussianBlur(frame, (11, 11), 0)
-    _,thresh = cv2.threshold(blur,100,255,cv2.THRESH_BINARY)
-    camera.show(frame)
+    _, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+    contours, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(opening, contours, -1, (255, 0, 0), 3)
+
     cv2.imshow('Thresh', thresh)
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow('Opening', opening)
+
     while cvwindows.event_loop():
         pass
 
