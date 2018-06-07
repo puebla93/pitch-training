8a245e9e1a50cbcc1d149060d9bfcaa1a55cf6f1
Javier Puebla <jpuebla1993@gmail.com>
2018-06-07 15:26:34 -0400

    adding *.json to .gitignore

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-import yaml
-from parse_args import args
+import json
+import glob
 from utils import Obj
 
 params = Obj(
@@ -21,11 +21,11 @@ def calibrate():
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
 
-    camera_index = args.camera
-    cap = cv2.VideoCapture(camera_index)
-    found = 0
-    while(found < 10):  # Here, 10 can be changed to whatever number you like to choose
-        ret, img = cap.read() # Capture frame-by-frame
+    images = glob.glob('chessboard_images/*.jpg')
+    found = 1
+    images.sort(key=lambda image: int(image[-5:-4]) if image[-6] == '/' else int(image[-6:-4]))
+    for fname in images:
+        img = cv2.imread(fname) # Capture frame-by-frame
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
@@ -39,38 +39,59 @@ def calibrate():
 
             # Draw and display the corners
             cv2.drawChessboardCorners(img, params.chessboard_size, corners, ret)
-            found += 1
-            cv2.imwrite('chessboard_images/'+str(found)+'.jpg', img)
-            print found
+            cv2.imwrite('chessboard_images/'+str(found)+'.png', img)
+        else:
+            to_print = fname[-5:] if fname[-6] == '/' else fname[-6:]
+            print to_print
 
+        found += 1
         cv2.imshow('img', img)
-        cv2.waitKey(0) if ret == True else cv2.waitKey(10)
+        cv2.waitKey(0)
         
-    # When everything done, release the capture
-    cap.release()
+    # When everything done
     cv2.destroyAllWindows()
 
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
+    mean_error = 0
+    for i in xrange(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+        mean_error += error
+    print("total error: {}".format(mean_error/len(objpoints)))
+
     return mtx, dist
 
-def save_matrix(mtx, dist):
+def save_matrix(mtx, dist, path):
     # It's very important to transform the matrix to list.
 
     data = {'camera_matrix': np.asarray(mtx).tolist(), 'dist_coeff': np.asarray(dist).tolist()}
 
-    with open("calibration.yaml", "w") as f:
-        yaml.dump(data, f)
+    with open(path, "w") as f:
+        json.dump(data, f)
 
-def load_matrix():
-    with open('calibration.yaml') as f:
-        loadeddict = yaml.load(f)
+def load_matrix(path):
+    with open(path) as f:
+        loadeddict = json.load(f)
 
     mtxloaded = loadeddict.get('camera_matrix')
     distloaded = loadeddict.get('dist_coeff')
     
     return mtxloaded, distloaded
 
+def save_chessboard_images():
+    camera_index = args.camera
+    cap = cv2.VideoCapture(camera_index)
+    i = 0
+    while i < 30:
+        ret, img = cap.read()
+        k = cv2.waitKey(1) & 0xFF
+        if k == 32:
+            cv2.imwrite('chessboard_images/'+str(i)+'.jpg', img)
+            i += 1
+        cv2.imshow('img', img)
+
 if __name__ == '__main__':
+    # save_chessboard_images()
     mtx, dist = calibrate()
-    save_matrix(mtx, dist)
+    save_matrix(mtx, dist, 'calibration.json')
